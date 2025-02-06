2297f12e70b646abe91a77ed9a4f4a45c5cce97e
Megan Richards <mr7401@log-2.hpc.nyu.edu>
2025-02-06 16:04:11 -0500

    support for wandb and/or local logging

---
+++ logger.py
@@ -0,0 +1,54 @@
+import os
+import wandb
+import logging
+
+def add_log_args(parser):
+    parser.add_argument("--logger", type=str, choices=["local", "wandb", "both", None], default=None, help="Log metrics to local only, wandb, or both")
+    parser.add_argument("--project", type=str, default = "test", help="project name for logging")
+    parser.add_argument("--log_dir", type=str, default = ".", help="Directory to save logs")
+    parser.add_argument("--run_name", type=str, default = ".", help="name of the run (category)")
+    return parser
+
+class Logger:
+    def __init__(self, logger="local", project="test", log_dir = ".", run_name = "test"): 
+        self.project = project
+        
+        # Set up WandB
+        if logger == "wandb" or logger == "both":
+            print("Setting up WandB")
+            self.log_to_wandb = True 
+            os.environ["WANDB_DIR"] = log_dir
+            wandb.init(project=project)
+            self.wandb_id = wandb.run.id
+        else: 
+            self.log_to_wandb = False
+        
+        # Set up local 
+        if logger == "local" or logger == "both":
+            print("Setting up local logging")
+            self.log_locally = True
+            self.local_logger = logging.getLogger(project)
+
+            pid = os.getpid()  # Get process ID
+            self.run_id = self.wandb_id if self.log_to_wandb else pid
+
+            os.makedirs(f"{log_dir}/outputs/{run_name}/{self.run_id}", exist_ok = True)
+            logging.basicConfig(filename=f"{log_dir}/outputs/{run_name}/{self.run_id}/local.log", level=logging.DEBUG)
+        else:
+            self.log_locally = False
+        
+        if not self.log_to_wandb and not self.log_locally:
+            raise Warning("No logging mode selected. To log results, select one of 'wandb', 'local', or 'both'")
+    
+    def log(self, metrics):
+        """Logs to wandb or writes to a file, based on the mode."""     
+        if self.log_to_wandb:
+            wandb.log(metrics)
+        if self.log_locally:
+            self.local_logger.info(metrics)
+    
+    def finish(self):
+        if self.log_to_wandb:
+            wandb.finish()
+        
+
--- run_bash.py
+++ run_bash.py
@@ -2,18 +2,20 @@ import os
 import pathlib
 import hydra
 import subprocess
-import logging
+import logging 
 from omegaconf import DictConfig
 
-log = logging.getLogger("run_bash")
-
 # config path set automatically but can also be manually changed
-#CONFIG_PATH = pathlib.Path(__file__).parent.resolve()
-#print(CONFIG_PATH)
+CONFIG_PATH = pathlib.Path(__file__).parent.resolve()
+print(CONFIG_PATH)
+os.environ['SSL_CERT_FILE']="/scratch/mr7401/projects/meta_comp/cacert.pem" # allows wandb to run in singularity
+os.environ['PYTHONPATH']="/scratch/mr7401/projects/meta_comp/:$PYTHONPATH" 
 
 @hydra.main(version_base=None, config_path='.', config_name="config")
 def launch(cfg: DictConfig):
-    log.info(cfg.command)
+    # Add the logging variables from config 
+    cfg.command += f" --logger={cfg.logger} --project={cfg.project} --log_dir={cfg.log_dir} --run_name={cfg.run_name}"
+    print(cfg.command)
     subprocess.run(cfg.command, shell=True)
 
 if __name__ == "__main__":
