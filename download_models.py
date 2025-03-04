1601b68f6c1f0b3652ac04b5d2fbcbe4d9f6890e
Megan Richards <mr7401@log-2.hpc.nyu.edu>
2025-03-04 15:58:31 -0500

    auto download model weights in repo

--- download_models.py
+++ download_models.py
@@ -1,35 +1,29 @@
-import argparse
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import os 
+from models.llms import AVAILABLE_MODELS 
 
 def download_models():
    
-    # Add your model downloading logic here
-    print("Downloading models...")
-    ### Downloading models 
-    names = [
-    "meta-llama/Llama-3.2-1B", "meta-llama/Llama-3.2-3B" ,
-    "meta-llama/Llama-3.1-8B", "meta-llama/Llama-3.1-405B", 
-    "gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl", 
-    "facebook/opt-125m", "facebook/opt-350m",  "facebook/opt-1.3B",  "facebook/opt-2.7B","facebook/opt-6.7B", "facebook/opt-13B", "facebook/opt-30B", "facebook/opt-66B"
-    ]
-    for name in names:
-        print("Trying to download model: ", name)
+    print("Fetching all models in repository...", flush=True)
+    huggingface_ids= {model_name: model(load_model=False).huggingface_id for model_name, model in AVAILABLE_MODELS.items()}
+
+    print("Starting download...", flush=True)
+    for class_name, name in huggingface_ids.items():
+        print("Trying to download model: ", name, flush=True)
         if os.path.exists(f"model_weights/{name}"):
-            print(f"Model {name} already exists. Skipping...")
-            continue
+            print(f"Model {name} already exists. Skipping...", flush=True)
         else: 
             try:
                 model = AutoModelForCausalLM.from_pretrained(name)
                 tokenizer = AutoTokenizer.from_pretrained(name)
                 model.save_pretrained(f"/scratch/mr7401/projects/meta_comp/model_weights/{name}")
                 tokenizer.save_pretrained(f"/scratch/mr7401/projects/meta_comp/model_weights/{name}")
-                print(f"Model and Tokenizer {name} downloaded successfully")
+                print(f"Model and Tokenizer {name} downloaded successfully", flush=True)
             except Exception as e:
-                print(f"Model {name} download failed")
-                print(e)
+                print(f"Model {name} download failed", flush=True)
+                print(e, flush=True)
                 continue
 
 if __name__ == "__main__":
-    print("Main Running")
+    print("Main Running", flush=True)
     download_models()
